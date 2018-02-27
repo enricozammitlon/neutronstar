c86f9e1dd2b91f84778c9a24555611af3034f3e1
Enrico <enrico.zammitl@gmail.com>
2018-02-27 16:35:43 +0000

    added c constant

--- neutronstar.py
+++ neutronstar.py
@@ -3,9 +3,11 @@ import numpy as np
 
 global G
 global MSOLAR
+global c
+c=299792458
 G=197.327*6.67259*(10**-45)
 MSOLAR=1.1157467*(10**60)
-rhoS=[(3.7*10**17)*1.6*10/(9*10**16)]
+rhoS=[((3.7*(10**17))*1.6*0.1)/(c**2)]
 
 def initConstants(rhoS):
     rZero=[]
