0317be40d384290ef9da8afdcebfb7fade93df3c
Enrico <enrico.zammitl@gmail.com>
2018-02-27 16:22:35 +0000

    Added Andrew's book chapter

---
+++ neutronstar.py
@@ -0,0 +1,18 @@
+import math
+import numpy as np
+
+global G
+global MSOLAR
+G=197.327*6.67259*(10**-45)
+MSOLAR=1.1157467*(10**60)
+rhoS=[(3.7*10**17)*1.6*10/(9*10**16)]
+
+def initConstants(rhoS):
+    rZero=[]
+    mZero=[]
+    for rhos in rhoS:
+        rZero.append(((10)**(-19))/(np.sqrt(rhos*G*4*np.pi)))
+        mZero.append((4*np.pi*rhos)/(((rhos*G*4*np.pi)**(3/2))*MSOLAR))
+    return rZero,mZero
+
+print(initConstants(rhoS))
