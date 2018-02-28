4ec3307c22ca3a3d31e280667cb76cd96641cbdc
smiles07 <charles.smith-10@student.manchester.ac.uk>
2018-02-28 15:31:48 +0000

    commented and checked
    The mass is not in terms of MeV/c2 , it is Mev, the c2 is brought over from E = mc2 ( for show).

--- neutronstar.py
+++ neutronstar.py
@@ -4,20 +4,22 @@ import numpy as np
 global G
 global MSOLAR
 global c
-c=299792458
-G=197.327*6.67259*(10**-45)
-MSOLAR=1.1157467*(10**60)
-rhoS=[((3.7*10**17)*10**(-45)*(3*10**8)**2)/(1.6*10**(-13))]
+c=299792458 # m/s
+G=197.327*6.67259*(10**-45) # not checked
+MSOLAR=1.1157467*(10**60) #Mev - Energy
+rhoS=[((3.7*10**17)*10**(-45)*(3*10**8)**2)/(1.6*10**(-13))] #MeVFm-3 - Energy desnity from Kgm-3
+#do we want rhos in kg and then another array where rhos are in energy density using this converstion? 
 
 def initConstants(rhoS):
     rZero=[]
     mZero=[]
     check=[]
     for rhos in rhoS:
-        rZero.append((10**-19)*(1/(np.sqrt(rhos*G*4*np.pi))))
-        mZero.append((4*np.pi*rhos)/(((rhos*G*4*np.pi)**(3/2))*MSOLAR))
-        check.append((G*4*np.pi*rhos)/(((rhos*G*4*np.pi)**(3/2)))/(1/(np.sqrt(rhos*G*4*np.pi))))
+        rZero.append((10**-19)*(1/(np.sqrt(rhos*G*4*np.pi)))) #with conversion factor for 10km
+        mZero.append((4*np.pi*rhos)/(((rhos*G*4*np.pi)**(3/2))*MSOLAR))#with convertion to terms of solar masses.
+        check.append((G*4*np.pi*rhos)/(((rhos*G*4*np.pi)**(3/2)))/(1/(np.sqrt(rhos*G*4*np.pi)))) #check according to chapter should be 1.
     return rZero,mZero,check
 print(initConstants(rhoS))
+#Im not sure these should return relatible numbers, but the units seem to check out. Mass is in terms of energy and density is energy density.
 
-#check caclulation should without MSOLAR or Km correction = 1
+#
