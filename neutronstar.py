6a7bd6e116133ffa0c7040310a13efa50e4ad755
smiles07 <charles.smith-10@student.manchester.ac.uk>
2018-02-28 16:11:38 +0000

    adding as i go

--- neutronstar.py
+++ neutronstar.py
@@ -1,4 +1,4 @@
-import math
+import sympy as sp
 import numpy as np
 
 global G
@@ -7,6 +7,7 @@ global c
 c=299792458 # m/s
 G=197.327*6.67259*(10**-45) # not checked
 MSOLAR=1.1157467*(10**60) #Mev - Energy
+MNEUTRON = 938.926 # Mev
 rhoS=[((3.7*10**17)*10**(-45)*(3*10**8)**2)/(1.6*10**(-13))] #MeVFm-3 - Energy desnity from Kgm-3
 #do we want rhos in kg and then another array where rhos are in energy density using this converstion? 
 
@@ -22,4 +23,15 @@ def initConstants(rhoS):
 print(initConstants(rhoS))
 #Im not sure these should return relatible numbers, but the units seem to check out. Mass is in terms of energy and density is energy density.
 
-#
+#solve equations
+
+#function for New pressure to new density
+
+def PressureToDensity(Pressure):
+    #P = 363.44 * n**2.54
+    n = (Pressure/363.44)**(1/2.54)
+    density = 236*n**2.54 +n*MNEUTRON
+    return density
+
+#rk4 to give new pressure. ( each new pressure )
+    
