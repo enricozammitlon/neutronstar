a10f33a17d4ca72a896c94366638944b9a5469c0
smiles07 <charles.smith-10@student.manchester.ac.uk>
2018-02-28 10:40:24 +0000

    checked over, this should be okay
    going to have a lok over the rk4 later

--- neutronstar.py
+++ neutronstar.py
@@ -7,14 +7,17 @@ global c
 c=299792458
 G=197.327*6.67259*(10**-45)
 MSOLAR=1.1157467*(10**60)
-rhoS=[(32.7*10**17)*1.6*10/(9*10**16)]
+rhoS=[((3.7*10**17)*10**(-45)*(3*10**8)**2)/(1.6*10**(-13))]
 
 def initConstants(rhoS):
     rZero=[]
     mZero=[]
+    check=[]
     for rhos in rhoS:
-        rZero.append(((10)**(-19))/(np.sqrt(rhos*G*4*np.pi)))
+        rZero.append((10**-19)*(1/(np.sqrt(rhos*G*4*np.pi))))
         mZero.append((4*np.pi*rhos)/(((rhos*G*4*np.pi)**(3/2))*MSOLAR))
-    return rZero,mZero
-
+        check.append((G*4*np.pi*rhos)/(((rhos*G*4*np.pi)**(3/2)))/(1/(np.sqrt(rhos*G*4*np.pi))))
+    return rZero,mZero,check
 print(initConstants(rhoS))
+
+#check caclulation should without MSOLAR or Km correction = 1
