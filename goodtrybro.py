7d404808b0f8a5b119cb69dbd7faa4ba6dd42fdb
Enrico <enrico.zammitl@gmail.com>
2018-03-02 13:33:33 +0000

    Fixed Callable error

--- goodtrybro.py
+++ goodtrybro.py
@@ -7,16 +7,7 @@ P=[10000]
 m=[4]
 r=[1]
 
-def rk4m(y,dy,x,h,rhos):
-    k1=dy(y,x,rhos)
-    k2=dy(y+h/2*k1,x+h/2,rhos)
-    k3=dy(y+h/2*k2,x+h/2,rhos)
-    k4=dy(y+h*k3,x+h,rhos)
-    y=y+h*(k1+2*k2+2*k3+k4)/6
-    x=x+h
-    return (x,y)
-
-def rk4P(y,dy,x,h,rhos):
+def rk4(y,dy,x,h,rhos):
     k1=dy(y,x,rhos)
     k2=dy(y+h/2*k1,x+h/2,rhos)
     k3=dy(y+h/2*k2,x+h/2,rhos)
@@ -39,30 +30,18 @@ def mderiv(m,rHat,densityHat):
 
 
 
-run= np.arange(1,10000)
 h=0.1
 
-for i in run:
-    print(m[i-1])
-    (ri,mi)=rk4m(m[i-1], mderiv(m[i-1],r[i-1],rho[i-1]), r[i-1], h, rho[i-1])
+for i in range(1,50):
+    print("Mass%6.2e : "%(m[i-1]))
+    (ri,mi)=rk4(m[i-1], mderiv , r[i-1], h, rho[i-1])
     r.append(ri)
     m.append(mi)
-    (ri,Pi)=rk4(P[i-1], Pderiv(m[i-1],r[i-1],rho[i-1]), r[i-1],h)
+    (ri,Pi)=rk4(P[i-1], Pderiv , r[i-1],h,rho[i-1])
     P.append(Pi)
-    rho.appendPressureToDensity(Pi)
-    print(Pi)
-    
-    
-    
+    rho.append(PressureToDensity(Pi))
+    print("Pressure%6.2e :"%Pi)
+
+
+
     #was working on this as practisse
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
